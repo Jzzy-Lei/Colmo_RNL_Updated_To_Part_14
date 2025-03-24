106b37c1cc142f71651cff0b2ee14d10b65756c9
Jzzy-Lei <colmojzzylei@gmail.com>
2025-03-25 06:38:52 +0800

    Updated up to Part 5

---
+++ client/src/components/tables/GendersTable.tsx
@@ -0,0 +1,30 @@
+const GendersTable = () => {
+  return (
+    <>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>GENDER</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>MALE</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>FEMALE</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>TRANSFORMERS</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default GendersTable;
--- server/routes/api.php
+++ server/routes/api.php
@@ -7,3 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(GenderController::class)->group(function () {
     Route::post('/storeGender', 'storeGender');
 });
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
