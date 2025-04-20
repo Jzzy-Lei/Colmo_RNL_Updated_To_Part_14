709dc10310ca9755471f216584e9b255ee118ca1
Jzzy-Lei <colmojzzylei@gmail.com>
2025-04-21 02:06:46 +0800

    Fetch and Update Data from Users in Edit (Action)

--- client/src/services/UserService.ts
+++ client/src/services/UserService.ts
@@ -2,10 +2,21 @@ import AxiosInstance from "../AxiosInstance";
 
 const UserService = {
     loadUsers: async () => {
-        return AxiosInstance.get('/loadUsers').then((response) => response).catch((error) => {throw error});
+        return AxiosInstance.get('/loadUsers')
+        .then((response) => response)
+        .catch((error) => {
+            throw error
+        });
     },
     storeUser: async (data: any) => {
         return AxiosInstance.post("/storeUser", data)
+        .then((response) => response)
+        .catch((error) => {
+            throw error;
+        });
+    },
+    updateUser: async (userId: number, data: any) => {
+        return AxiosInstance.put(`/updateUser/${userId}`, data)
         .then((response) => response)
         .catch((error) => {
             throw error;
